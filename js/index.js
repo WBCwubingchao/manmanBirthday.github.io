6684b11a14524465745d2e203c00cde0098fc763
paulpig <494824017@qq.com>
2017-08-22 16:12:18 +0800

    swiper function!

---
+++ js/index.js
@@ -0,0 +1,43 @@
+var currentNum=1;
+var nextNum=2;
+var centerName=".pic0";
+var rightName=".pic1";
+
+$(document).ready(function(){
+
+	// 定时器调用函数，循环进行！！
+	setInterval(setCurrentPosition, 8000);
+	// setCurrentPosition();
+
+});
+
+
+function setCurrentPosition(){
+	$(centerName).removeAttr('id','center_picture');
+	$(rightName).removeAttr('id','right_picture');
+	// 两张图像位置设置好
+	centerName=".pic"+currentNum;
+	// alert(centerName);
+	// $(".pic0").addClass("center_picture");
+	$(centerName).attr('id','center_picture');
+
+	if (currentNum==9){
+		nextNum=0;
+	}
+	else{
+		nextNum=currentNum+1;
+	}
+
+	rightName=".pic"+nextNum;
+	// alert(rightName);
+	$(rightName).attr('id','right_picture');
+
+	currentNum=nextNum;
+}
+
+
+function swiperPic(center_pic,right_pic){
+	// 移动两张图像
+	// center_pic.
+	// right_pic.
+}
